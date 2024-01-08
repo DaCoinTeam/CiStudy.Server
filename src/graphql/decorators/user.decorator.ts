4483f15151a09e99445db834daabc2984be8f8d3
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-01-09 05:48:54 +0700

    add jwt protector

--- src/graphql/decorators/user.decorator.ts
+++ src/graphql/decorators/user.decorator.ts
@@ -1,11 +1,6 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common"
 import { GqlExecutionContext } from "@nestjs/graphql"
 
-export const UserRestful = createParamDecorator((_, ctx: ExecutionContext) => {
-	const request = ctx.switchToHttp().getRequest()
-	return request.user
-})
-
 export const User = createParamDecorator(
 	(_, context: ExecutionContext) => {
 		const ctx = GqlExecutionContext.create(context)
