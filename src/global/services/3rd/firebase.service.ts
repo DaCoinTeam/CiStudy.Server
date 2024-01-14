645bf10df804c27c139d0936d639b90b02141f1c
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-01-14 13:59:42 +0700

    upload firebase (without tested)

--- src/global/services/3rd/firebase.service.ts
+++ src/global/services/3rd/firebase.service.ts
@@ -4,12 +4,14 @@ import * as admin from "firebase-admin"
 import firebase, { ServiceAccount } from "firebase-admin"
 import { Auth } from "firebase-admin/lib/auth/auth"
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier"
-import { Storage } from "firebase-admin/lib/storage/storage"
+import { Bucket } from "@google-cloud/storage/build/cjs/src"
+import { v4 as uuidv4 } from "uuid"
+import { getDownloadURL } from "firebase-admin/storage"
 
 @Injectable()
 export default class FirebaseService {
 	auth: Auth
-	storage: Storage
+	bucket: Bucket
 	constructor() {
 		const adminConfig: ServiceAccount = {
 			projectId: thirdPartyConfig().firebase.projectId,
@@ -21,10 +23,20 @@ export default class FirebaseService {
 		})
 
 		this.auth = firebase.auth(app)
-		this.storage = firebase.storage(app)
+		this.bucket = firebase.storage(app).bucket("supple-century-363201.appspot.com")
 	}
 
 	async verifyGoogleAccessToken(token: string) : Promise<DecodedIdToken> {
 		return await this.auth.verifyIdToken(token)
+	}
+
+	async uploadFile(file: File) {
+		const arrayBuffer = await file.arrayBuffer()
+		const buffer = Buffer.from(arrayBuffer)
+		const fileName = uuidv4()
+		
+		const created = this.bucket.file(fileName)
+		await created.save(buffer)
+		return getDownloadURL(created)
 	}
 }
