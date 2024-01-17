2eabaac445ae1b35ab94030b4124973d5519d934
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-01-17 16:02:17 +0700

    12312

--- src/graphql/course/models/find-one/input.model.ts
+++ src/graphql/course/models/find-one/input.model.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ID, InputType } from "@nestjs/graphql"
+import { IsUUID } from "class-validator"
 
 @InputType()
 export default class FindOneInput {
   @Field(type => ID)
+  @IsUUID()
   	courseId: string
 }
